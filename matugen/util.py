5774f5cf24d667739110ccb3af607c86f407e024
InioX <justimnix@gmail.com>
2023-03-18 22:24:53 +0100

    initial commit

---
+++ matugen/util.py
@@ -0,0 +1,177 @@
+import os
+import re
+import logging
+import pathlib
+from material_color_utilities_python import Image, themeFromImage
+from rich.logging import RichHandler
+from configparser import ConfigParser
+from argparse import Namespace, ArgumentParser
+from pathlib import Path
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "wallpaper",
+        help="the wallpaper that will be used",
+        type=str
+    )
+
+    parser.add_argument(
+        "-l", "--lightmode",
+        help="specify whether to use light mode",
+        action="store_true"
+    )
+    args: Namespace = parser.parse_args()
+    return args
+
+
+def setup_logging():
+    FORMAT = "%(message)s"
+    logging.basicConfig(
+        level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    )
+
+    log = logging.getLogger("rich")
+    return log
+
+
+log = setup_logging()
+
+
+def reload_apps():
+    log.info("Restarting waybar")
+    os.system("killall -SIGUSR2 waybar")
+
+    log.info("Restarting GTK")
+    os.system("gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark")
+    
+    log.info("Restarting kitty")
+    os.system("killall -SIGUSR1 kitty")
+
+
+def set_wallpaper(path: str):
+    log.info("Setting wallpaper with swaybg")
+    os.system(f"swaybg -i {path}&")
+
+class Color:
+    @staticmethod
+    def rgb_to_hex(rgb: int) -> str:
+        return '%02x%02x%02x' % rgb
+
+    def hex_to_rgb(hexa: str):
+        return tuple(int(hexa[i:i+2], 16)  for i in (0, 2, 4))
+
+    @staticmethod
+    def dec_to_rgb(decimal_value: int) -> int:
+        red = (decimal_value >> 16) & 255
+        green = (decimal_value >> 8) & 255
+        blue = decimal_value & 255
+
+        return red, green, blue
+
+
+class Config:
+    @staticmethod
+    def read(filename: str):
+        config = ConfigParser()
+        try:
+            config.read(filename)
+        except OSError as err:
+            logging.exception(f"Could not open {err.filename}")
+        else:
+            logging.info(
+                f"Loaded {len(config.sections())} templates from config file"
+            )
+            return config
+
+    @staticmethod
+    def generate(scheme: dict, config: ConfigParser, wallpaper: str) -> dict:
+        for item in config.sections():
+            num = 0
+            template_name = config[item].name
+            template_path = Path(config[item]["template_path"]).expanduser()
+            output_path = Path(config[item]["output_path"]).expanduser()
+
+            try:
+                with open(template_path, "r") as input:  # Template file
+                    input_data = input.read()
+            except OSError as err:
+                logging.exception(f"Could not open {err.filename}")
+                num += 1
+                return
+
+            output_data = input_data
+
+            for key, value in scheme.items():
+                pattern = f"@{{{key}}}"
+                pattern_hex = f"@{{{key}.hex}}"
+                pattern_rgb = f"@{{{key}.rgb}}"
+                pattern_wallpaper = "@{wallpaper}"
+
+                hex_stripped = value[1:]
+                rgb_value = f"rgb{Color.hex_to_rgb(hex_stripped)}"
+
+                print(f"Stripped: {hex_stripped}")
+                print(f"Hex: {value}")
+                print(f"Rgb: {rgb_value}")
+                print(f"Wallpaper: {wallpaper}")
+
+                output_data = re.sub(pattern, hex_stripped, output_data)
+                output_data = re.sub(pattern_hex, value, output_data)
+                output_data = re.sub(pattern_rgb, rgb_value, output_data)
+                output_data = re.sub(pattern_wallpaper, wallpaper, output_data)
+                num += 1
+
+            try:
+                with open(output_path, "w") as output:
+                    output.write(output_data)
+            except OSError as err:
+                logging.exception(
+                    f"Could not write {template_name} template to {err.filename}")
+            else:
+                log.info(
+                    f"Exported {template_name} template to {output_path}")
+
+
+class Theme:
+    @staticmethod
+    def get(image: str):
+        log.info(f"Using image {image}")
+        img = Image.open(image)
+        basewidth = 64
+        wpercent = (basewidth/float(img.size[0]))
+        hsize = int((float(img.size[1])*float(wpercent)))
+        img = img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+
+        return themeFromImage(img)
+
+class Scheme():
+    def __init__(self, theme: str, lightmode: bool):
+        if lightmode:
+            log.info("Using light scheme")
+            self.scheme_dict = theme["schemes"]["light"].props
+        else:
+            log.info("Using dark scheme")
+            self.scheme_dict = theme["schemes"]["dark"].props
+
+    def get(self) -> dict:
+        return self.scheme_dict
+
+    def to_rgb(self) -> dict:
+        scheme = self.scheme_dict
+
+        for key, value in scheme.items():
+            scheme[key] = Color.dec_to_rgb(value)
+        return scheme
+
+    def to_hex(self) -> dict:
+        scheme = self.scheme_dict
+
+        # Need to convert to rgb first
+        self.to_rgb()
+
+        for key, value in scheme.items():
+            scheme[key] = "#{value}".format(value=Color.rgb_to_hex(value))
+        return scheme
