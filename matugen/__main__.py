42b29c1b1b40aaaf1b2b12c06e3991082e4c5b88
InioX <justimnix@gmail.com>
2023-04-16 17:15:24 +0200

    refactor: :truck: rename main.py to __main__.py

---
+++ matugen/__main__.py
@@ -0,0 +1,26 @@
+from matugen.util import (
+    Theme,
+    Scheme,
+    Config,
+    set_wallpaper,
+    reload_apps,
+    parse_arguments
+)
+
+def get_scheme(args):
+    scheme = Scheme(Theme.get(args.wallpaper), args.lightmode)
+    return scheme.to_hex()
+
+
+def main():
+    args = parse_arguments()
+    scheme = get_scheme(args)
+    conf = Config.read("config.ini")
+
+    Config.generate(scheme, conf, args.wallpaper)
+    reload_apps()
+    set_wallpaper(args.wallpaper)
+
+
+if __name__ == "__main__":
+    main()
