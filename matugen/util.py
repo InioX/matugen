1a4c8079fdd0fb22f5b96a899dd6e054253f470f
InioX <justimnix@gmail.com>
2023-04-21 19:06:15 +0200

    refactor: :art: add type hint to hex_to_rgb

--- matugen/util.py
+++ matugen/util.py
@@ -95,7 +95,7 @@ class Color:
     def rgb_to_hex(rgb: int) -> str:
         return '%02x%02x%02x' % rgb
 
-    def hex_to_rgb(hexa: str):
+    def hex_to_rgb(hexa: str) -> tuple[int, int, int]:
         return tuple(int(hexa[i:i+2], 16) for i in (0, 2, 4))
 
     @staticmethod
