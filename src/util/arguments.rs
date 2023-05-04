bb0751ef464de6dc909db5264f8dde57bda3ebcc
InioX <justimnix@gmail.com>
2023-05-04 20:39:30 +0200

    refactor: remove dead code

--- src/util/arguments.rs
+++ src/util/arguments.rs
@@ -1,28 +1,6 @@
 use clap::{arg, ArgAction, Command, Parser, Subcommand};
 use std::path::PathBuf;
 
-// #[derive(Default, Parser, Debug)]
-// #[command(name = "matugen")]
-// #[command(about = "A material color you color generator", long_about = None)]
-// pub struct Arguments {
-//     /// The image to use for generating colorscheme
-//     pub image: String,
-
-//     /// The config path to use for loading templates.
-//     #[arg(short, long)]
-//     pub config: Option<PathBuf>,
-
-//     /// Whether to use lightmode for the colorscheme
-//     #[arg(short, long, action=ArgAction::SetTrue)]
-//     pub lightmode: Option<bool>,
-
-//     #[arg(short, long, action=ArgAction::SetTrue)]
-//     pub verbose: Option<bool>,
-
-//     #[arg(short, long, action=ArgAction::SetTrue)]
-//     pub quiet: Option<bool>,
-// }
-
 #[derive(Parser, Debug)]
 #[command(version, long_about = None)]
 pub struct Cli {
