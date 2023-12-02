f0287da4815f9f3585f61b18cb812be6e6d165ba
InioX <justimnix@gmail.com>
2023-12-02 17:09:24 +0100

    feat(wallpaper): add error is the program to set wallpaper is not in PATH

--- src/wallpaper/unix.rs
+++ src/wallpaper/unix.rs
@@ -10,6 +10,7 @@ use crate::util::{
 
 #[cfg(any(target_os = "linux", target_os = "netbsd"))]
 pub fn set(config: &ConfigFile, path: &String) -> Result<(), Report> {
+    info!("Setting wallpaper...");
     let wallpaper_tool = match &config.config.wallpaper_tool {
         Some(wallpaper_tool) => wallpaper_tool,
         None => {
@@ -21,7 +22,7 @@ pub fn set(config: &ConfigFile, path: &String) -> Result<(), Report> {
 
     match wallpaper_tool {
         WallpaperTool::Swaybg => set_wallaper_swaybg(path),
-        WallpaperTool::Swww => set_wallaper_swwww(config, path),
+        WallpaperTool::Swww => set_wallaper_swww(config, path),
         WallpaperTool::Nitrogen => set_wallaper_nitrogen(path),
         WallpaperTool::Feh => set_wallaper_feh(config, path),
     }
@@ -36,12 +37,21 @@ fn set_wallaper_swaybg(path: &String) -> Result<(), Report> {
     cmd.arg("-i");
     cmd.arg(path);
 
-    cmd.spawn()?;
+    match cmd.spawn() {
+        Ok(_) => info!("Successfully set the wallpaper with <blue>swaybg</>"),
+        Err(e) => {
+            if let std::io::ErrorKind::NotFound = e.kind() {
+                error!("Failed to set wallpaper, the program <red>swaybg</> was not found in PATH!")
+            } else {
+                error!("Some error(s) occured while setting wallpaper!");
+            }
+        },
+    };
     Ok(())
 }
 
 #[cfg(any(target_os = "linux", target_os = "netbsd"))]
-fn set_wallaper_swwww(config: &ConfigFile, path: &String) -> Result<(), Report> {
+fn set_wallaper_swww(config: &ConfigFile, path: &String) -> Result<(), Report> {
     let mut binding = Command::new("swww");
     let cmd = binding.stdout(Stdio::null()).stderr(Stdio::null());
     cmd.arg("img");
@@ -53,7 +63,16 @@ fn set_wallaper_swwww(config: &ConfigFile, path: &String) -> Result<(), Report> 
         }
     }
 
-    cmd.spawn()?;
+    match cmd.spawn() {
+        Ok(_) => info!("Successfully set the wallpaper with <blue>swww</>"),
+        Err(e) => {
+            if let std::io::ErrorKind::NotFound = e.kind() {
+                error!("Failed to set wallpaper, the program <red>swww</> was not found in PATH!")
+            } else {
+                error!("Some error(s) occured while setting wallpaper!");
+            }
+        },
+    };
     Ok(())
 }
 
@@ -63,7 +82,16 @@ fn set_wallaper_nitrogen(path: &String) -> Result<(), Report> {
     let cmd = binding.stdout(Stdio::null()).stderr(Stdio::null());
     cmd.arg(path);
 
-    cmd.spawn()?;
+    match cmd.spawn() {
+        Ok(_) => info!("Successfully set the wallpaper with <blue>nitrogen</>"),
+        Err(e) => {
+            if let std::io::ErrorKind::NotFound = e.kind() {
+                error!("Failed to set wallpaper, the program <red>nitrogen</> was not found in PATH!")
+            } else {
+                error!("Some error(s) occured while setting wallpaper!");
+            }
+        },
+    };;
     Ok(())
 }
 
@@ -82,6 +110,15 @@ fn set_wallaper_feh(config: &ConfigFile, path: &String) -> Result<(), Report> {
 
     cmd.arg(path);
 
-    cmd.spawn()?;
+    match cmd.spawn() {
+        Ok(_) => info!("Successfully set the wallpaper with <blue>feh</>"),
+        Err(e) => {
+            if let std::io::ErrorKind::NotFound = e.kind() {
+                error!("Failed to set wallpaper, the program <red>feh</> was not found in PATH!")
+            } else {
+                error!("Some error(s) occured while setting wallpaper!");
+            }
+        },
+    };
     Ok(())
 }
