7f24d8a944926d881a6d548e3673a7a3b85fe0d3
DaniD3v <sch220233@spengergasse.at>
2024-05-03 17:16:15 +0200

    formatted with alejandra

--- flake.nix
+++ flake.nix
@@ -4,20 +4,23 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
   };
-  outputs = { self, nixpkgs, systems }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs (import systems);
-      pkgsFor = nixpkgs.legacyPackages;
-    in {
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./. { };
-      });
-      devShells = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./shell.nix { };
-      });
-      nixosModules = {
-        matugen = import ./module.nix self;
-        default = self.nixosModules.matugen;
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs (import systems);
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    packages = forAllSystems (system: {
+      default = pkgsFor.${system}.callPackage ./. {};
+    });
+    devShells = forAllSystems (system: {
+      default = pkgsFor.${system}.callPackage ./shell.nix {};
+    });
+    nixosModules = {
+      matugen = import ./module.nix self;
+      default = self.nixosModules.matugen;
     };
+  };
 }
