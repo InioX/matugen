a2b075a989b47f6f9da65f9d7195f2f626d6a63a
ini <81521595+InioX@users.noreply.github.com>
2024-11-06 17:17:33 +0100

    Update module.nix
    Co-authored-by: ys5g <136090707+ys5g@users.noreply.github.com>

--- module.nix
+++ module.nix
@@ -13,6 +13,23 @@ in {
   options.programs.matugen = {
     enable = lib.mkEnableOption "Matugen declarative theming";
 
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = matugen.packages.${pkgs.system}.default;
+      description = "The matugen package to use";
+    };
+    
+    extraPackages = lib.mkOption {
+      type = with lib.types; listOf package;
+      default = [];
+
+      example = lib.literalExpression ''with pkgs; [
+        sway # For swaymsg
+        glib # For gsettings
+      ]'';
+      description = "Extra package available to matugen while generating templates";
+    };
+    
     wallpaper = lib.mkOption {
       type = with lib.types; nullOr path;
       default = osCfg.wallpaper or null;
