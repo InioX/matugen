c708abab2f133711e5e3c70fa773bb587c053b51
InioX <justimnix@gmail.com>
2025-03-23 16:53:49 +0100

    feat(gui): add images tab

--- src/gui/init.rs
+++ src/gui/init.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{ffi::OsStr, path::PathBuf};
 
 #[cfg(feature = "ui")]
 use indexmap::IndexMap;
@@ -20,6 +20,7 @@ use crate::{template::TemplateFile, util::arguments::Cli, State};
 #[cfg(feature = "ui")]
 #[derive(PartialEq)]
 pub enum Tabs {
+    Images,
     Settings,
     Colors,
 }
@@ -33,12 +34,33 @@ pub struct MyApp {
     colors: ColorsMap,
     selected_tab: Tabs,
     app: State,
+    images_vec: Vec<PathBuf>,
+    images_folder: Option<PathBuf>,
 }
 
 #[cfg(feature = "ui")]
 pub struct ColorsMap {
     pub light: Option<IndexMap<String, Color32>>,
     pub dark: Option<IndexMap<String, Color32>>,
+}
+
+fn get_images_in_folder(folder_path: &PathBuf) -> Vec<PathBuf> {
+    let valid_extensions = ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "webp"];
+
+    std::fs::read_dir(folder_path)
+        .ok()
+        .into_iter()
+        .flatten()
+        .flatten()
+        .map(|entry| entry.path())
+        .filter(|path| path.is_file())
+        .filter(|path| {
+            path.extension()
+                .and_then(|ext| ext.to_str())
+                .map(|ext| valid_extensions.contains(&ext.to_lowercase().as_str()))
+                .unwrap_or(false)
+        })
+        .collect()
 }
 
 // FIXME: Cleanup code, reorganize stuff into its own functions
@@ -55,12 +77,15 @@ impl MyApp {
                 dark: None,
             },
             selected_tab: Tabs::Settings,
+            images_vec: vec![],
+            images_folder: None,
         }
     }
     fn body(&mut self, ui: &mut Ui) {
         match self.selected_tab {
             Tabs::Settings => self.settings(ui),
             Tabs::Colors => self.colors(ui),
+            Tabs::Images => self.images(ui),
         }
     }
 
@@ -73,7 +98,7 @@ impl MyApp {
             ));
         });
         ui.label("Scheme type");
-        egui::ComboBox::from_label("Select one!")
+        egui::ComboBox::from_label("")
             .selected_text(format!("{:?}", self.app.args.r#type.unwrap()))
             .show_ui(ui, |ui| {
                 ui.selectable_value(
@@ -172,18 +197,78 @@ impl MyApp {
         });
     }
 
+    fn images(&mut self, ui: &mut Ui) {
+        if self.images_vec.is_empty() {
+            ui.label("No image folder selected or no images to show.");
+        } else {
+            ui.with_layout(
+                egui::Layout::left_to_right(egui::Align::Max).with_cross_justify(true),
+                |ui| {
+                    self.show_images(ui);
+                },
+            );
+        }
+    }
+
+    fn show_images(&mut self, ui: &mut Ui) {
+        egui::ScrollArea::vertical().show(ui, |ui| {
+            egui::Grid::new("image_grid")
+                .num_columns(3) // Set number of columns
+                .spacing([10.0, 10.0]) // Spacing between items
+                .show(ui, |ui| {
+                    for (i, path) in self.images_vec.clone().iter().enumerate() {
+                        let img_widget = egui::Image::from_uri(format!(
+                            "file://{}",
+                            path.clone().into_os_string().into_string().unwrap()
+                        ))
+                        .max_height(300.0)
+                        .max_width(300.0)
+                        .maintain_aspect_ratio(true)
+                        .fit_to_original_size(1.0);
+
+                        if ui.add(img_widget.sense(egui::Sense::click())).clicked() {
+                            self.selected_file = Some(path.to_path_buf());
+                            self.run();
+                        }
+
+                        if (i + 1) % 3 == 0 {
+                            ui.end_row();
+                        }
+                    }
+                });
+        });
+    }
+
+    fn update_images_tab(&mut self, ui: &mut Ui, image_folder: Option<PathBuf>) {
+        if image_folder == Some("".into()) || image_folder.is_none() {
+            return;
+        }
+        let images = get_images_in_folder(&image_folder.as_ref().unwrap());
+        self.images_vec = images;
+    }
+
     fn top_buttons(&mut self, ui: &mut Ui) {
+        if ui.button("Image Folder").clicked() {
+            if let Some(path) = rfd::FileDialog::new().pick_folder() {
+                self.update_images_tab(ui, Some(path))
+            }
+        }
         if ui.button("Select image").clicked() {
             if let Some(path) = rfd::FileDialog::new().pick_file() {
                 self.selected_file = Some(path);
             }
         }
         if ui.button("Run").clicked() {
-            if self.selected_file != Some("".into()) || self.selected_file.is_some() {
-                self.update_colors_tab();
-                self.generate_tempalates();
-            };
+            self.run()
         }
+    }
+
+    fn run(&mut self) {
+        if self.selected_file == Some("".into()) || self.selected_file.is_none() {
+            return;
+        };
+        self.generate_tempalates();
+        self.update_colors_tab();
     }
 
     fn generate_tempalates(&mut self) {
@@ -225,9 +310,11 @@ fn argb_to_color32(color: &Argb) -> Color32 {
 #[cfg(feature = "ui")]
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui_extras::install_image_loaders(ctx);
         egui::TopBottomPanel::top("my_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.horizontal(|ui| {
+                    ui.selectable_value(&mut self.selected_tab, Tabs::Images, "Images");
                     ui.selectable_value(&mut self.selected_tab, Tabs::Settings, "Settings");
                     ui.selectable_value(&mut self.selected_tab, Tabs::Colors, "Colors");
                 });
