735fb4fd2922c8cd31a67e49d63cca403a69a4ea
InioX <justimnix@gmail.com>
2025-06-18 04:39:08 +0200

    wip: kinda working for loop

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -37,6 +37,24 @@ pub(crate) enum Expression<'src> {
         keyword: Box<SpannedExpr<'src>>,
         filters: Vec<SpannedExpr<'src>>,
     },
+    ForLoop {
+        var: SpannedValue,
+        list: Box<SpannedExpr<'src>>,
+        body: Vec<Box<SpannedExpr<'src>>>,
+    },
+    Raw {
+        value: String,
+    },
+}
+
+impl<'src> Expression<'src> {
+    pub fn as_keywords(&self) -> Option<&Vec<&'src str>> {
+        if let Expression::Keyword { keywords } = self {
+            Some(keywords)
+        } else {
+            None
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -153,18 +171,20 @@ impl Engine {
             };
         }
 
+        dbg!(&current);
+
         current
     }
 
     pub fn generate_templates(&self) {
         let (res, errs) = self.parser().parse(self.src.trim()).into_output_errors();
 
-        let mut changed_src: &mut String = &mut self.src.clone();
+        let mut changed_src: String = String::new();
 
         match res {
             Some(exprs) => {
-                for e in exprs.into_iter().rev() {
-                    self.get_keyword(&mut changed_src, e);
+                for e in exprs.into_iter() {
+                    self.build_string(&mut changed_src, *e);
                 }
             }
             None => {}
@@ -190,12 +210,12 @@ impl Engine {
         });
     }
 
-    fn get_keyword(&self, src: &mut String, expr: SpannedExpr) {
-        let range = expr.span.into_range();
+    fn build_string(&self, src: &mut String, expr: SpannedExpr) {
+        let _range = expr.span.into_range();
 
         match expr.expr {
             Expression::Keyword { keywords } => {
-                src.replace_range(range, &self.get_replacement(keywords));
+                src.push_str(&self.get_replacement(keywords));
             }
             Expression::KeywordWithFilters { keyword, filters } => {
                 let keywords = match keyword.expr {
@@ -203,14 +223,20 @@ impl Engine {
                     _ => panic!(""),
                 };
 
-                src.replace_range(
-                    range,
-                    &self.get_replacement_filter(keywords, filters).into(),
-                );
+                src.push_str(&self.get_replacement_filter(keywords, filters).into());
             }
-            _ => {
-                println!("other")
+            Expression::Raw { value } => {
+                src.push_str(&value);
             }
+            Expression::ForLoop { var, list, body } => {
+                let values = match list.expr.as_keywords() {
+                    Some(v) => self.resolve_path(v.iter().copied()),
+                    None => unreachable!(),
+                };
+
+                dbg!(values);
+            }
+            _ => {}
         }
     }
 
@@ -441,93 +467,149 @@ impl Engine {
 
     fn parser<'src>(
         &self,
-    ) -> impl Parser<'src, &'src str, Vec<SpannedExpr<'src>>, extra::Err<Rich<'src, char>>> {
-        let dotted_ident = text::ident()
-            .separated_by(just('.'))
-            .at_least(1)
-            .collect::<Vec<&'src str>>()
-            .map_with(|v, e| SpannedExpr {
-                expr: Expression::Keyword { keywords: v },
-                span: e.span(),
-            });
+    ) -> impl Parser<'src, &'src str, Vec<Box<SpannedExpr<'src>>>, extra::Err<Rich<'src, char>>>
+    {
+        recursive(|expr| {
+            let dotted_ident = text::ident()
+                .separated_by(just('.'))
+                .at_least(1)
+                .collect::<Vec<&'src str>>()
+                .map_with(|v, e| SpannedExpr {
+                    expr: Expression::Keyword { keywords: v },
+                    span: e.span(),
+                });
 
-        let float = text::int(10)
-            .then_ignore(just('.'))
-            .then(text::int(10))
-            .map(|(int_part, frac_part)| {
-                let parsed = format!("{}.{}", int_part, frac_part)
-                    .parse::<f64>()
-                    .unwrap();
-                Value::Float(parsed)
-            });
+            let float = text::int(10)
+                .then_ignore(just('.'))
+                .then(text::int(10))
+                .map(|(int_part, frac_part)| {
+                    let parsed = format!("{}.{}", int_part, frac_part)
+                        .parse::<f64>()
+                        .unwrap();
+                    Value::Float(parsed)
+                });
 
-        let int = text::int(10).map(|s: &str| Value::Int(s.parse::<i64>().unwrap()));
+            let int = text::int(10).map(|s: &str| Value::Int(s.parse::<i64>().unwrap()));
 
-        let ident = text::ident().map(|s: &str| Value::Ident(s.to_string()));
+            let ident = text::ident().map(|s: &str| Value::Ident(s.to_string()));
 
-        let arg = float
-            .or(int)
-            .or(ident)
-            .map_with(|value, e| SpannedValue::new(value, e.span()));
+            let spanned_ident = ident.map_with(|value, e| SpannedValue::new(value, e.span()));
 
-        let filter = text::ident()
-            .then(
-                just(':')
-                    .padded()
-                    .ignore_then(
-                        arg.padded()
-                            .separated_by(just(',').padded())
-                            .collect::<Vec<SpannedValue>>(),
-                    )
-                    .or_not(),
-            )
-            .map_with(|(name, args), e| SpannedExpr {
-                expr: Expression::Filter {
-                    name,
-                    args: args.unwrap_or_default(),
-                },
-                span: e.span(),
-            });
+            let arg = float
+                .or(int)
+                .or(ident)
+                .map_with(|value, e| SpannedValue::new(value, e.span()));
 
-        let filters = just('|')
-            .padded()
-            .ignore_then(filter.padded())
-            .repeated()
-            .collect::<Vec<_>>();
-
-        let full_expr = dotted_ident.then(filters).map(|(keyword, filters)| {
-            if filters.is_empty() {
-                keyword
-            } else {
-                let span = SimpleSpan::new(
-                    (),
-                    keyword.span.start
-                        ..filters
-                            .last()
-                            .map(|f| f.span.end)
-                            .unwrap_or(keyword.span.end),
-                );
-                dbg!(&span);
-                SpannedExpr {
-                    expr: Expression::KeywordWithFilters {
-                        keyword: Box::new(keyword),
-                        filters,
+            let filter = text::ident()
+                .then(
+                    just(':')
+                        .padded()
+                        .ignore_then(
+                            arg.padded()
+                                .separated_by(just(',').padded())
+                                .collect::<Vec<SpannedValue>>(),
+                        )
+                        .or_not(),
+                )
+                .map_with(|(name, args), e| SpannedExpr {
+                    expr: Expression::Filter {
+                        name,
+                        args: args.unwrap_or_default(),
                     },
-                    span,
-                }
-            }
-        });
+                    span: e.span(),
+                });
 
-        let keyword_full = just(self.syntax.keyword_left)
-            .padded()
-            .ignore_then(full_expr)
-            .padded()
-            .then_ignore(just(self.syntax.keyword_right))
-            .map_with(|expr, e| SpannedExpr {
-                expr: expr.expr,
-                span: e.span(),
+            let filters = just('|')
+                .padded()
+                .ignore_then(filter.padded())
+                .repeated()
+                .collect::<Vec<_>>();
+
+            let full_expr = dotted_ident.then(filters).map(|(keyword, filters)| {
+                if filters.is_empty() {
+                    keyword
+                } else {
+                    let span = SimpleSpan::new(
+                        (),
+                        keyword.span.start
+                            ..filters
+                                .last()
+                                .map(|f| f.span.end)
+                                .unwrap_or(keyword.span.end),
+                    );
+                    SpannedExpr {
+                        expr: Expression::KeywordWithFilters {
+                            keyword: Box::new(keyword),
+                            filters,
+                        },
+                        span,
+                    }
+                }
             });
 
-        keyword_full.padded().repeated().collect()
+            let keyword_full = just(self.syntax.keyword_left)
+                .padded()
+                .ignore_then(full_expr)
+                .padded()
+                .then_ignore(just(self.syntax.keyword_right))
+                .map_with(|expr, e| {
+                    Box::new(SpannedExpr {
+                        expr: expr.expr,
+                        span: e.span(),
+                    })
+                });
+
+            let for_end = just("endfor")
+                .padded()
+                .delimited_by(just(self.syntax.block_left), just(self.syntax.block_right));
+
+            let raw = any()
+                .and_is(
+                    just(self.syntax.keyword_left[0])
+                        .or(just(self.syntax.block_left[0]))
+                        .not(),
+                )
+                .repeated()
+                .at_least(1)
+                .collect::<String>()
+                .map_with(|text, span| {
+                    Box::new(SpannedExpr {
+                        expr: Expression::Raw {
+                            value: text, // or use Box::leak(text.into_boxed_str())
+                        },
+                        span: span.span(),
+                    })
+                });
+
+            let for_loop = just(self.syntax.block_left)
+                .map_with(|expr, e| (expr, e.span()))
+                .padded()
+                .ignore_then(just("for"))
+                .padded()
+                .ignore_then(spanned_ident)
+                .padded()
+                .then_ignore(just("in"))
+                .padded()
+                .then(dotted_ident)
+                .padded()
+                .then_ignore(just(self.syntax.block_right))
+                .padded()
+                .then(raw.or(expr).repeated().collect())
+                .then_ignore(for_end)
+                .map_with(|((var, list), body), e| {
+                    Box::new(SpannedExpr {
+                        expr: Expression::ForLoop {
+                            var,
+                            list: Box::new(list),
+                            body,
+                        },
+                        span: e.span(),
+                    })
+                });
+
+            raw.or(keyword_full).or(for_loop)
+        })
+        .repeated()
+        .collect::<Vec<Box<SpannedExpr<'src>>>>()
     }
 }
